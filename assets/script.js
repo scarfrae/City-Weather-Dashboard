2fdb242004551b08eb601a38cd2d88698a3f1910
stirfish <sterlinglc123@gmail.com>
2022-07-18 17:40:29 -0700

    working on inital javascript

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+//web api link
+
+//global query selectors
+//button link to html
+var button = document.querySelector('button');
+//input link to html
+var cityInput = document.querySelector('citySearch');
+//city header link to html
+var cityName = document.querySelector('name');
+//city temp link to html
+var cityTemp = document.querySelector('temperature');
+//city wind link to html
+var cityWind = document.querySelector('wind');
+//city humidity link to html
+var cityHumidity = document.querySelector('humidity');
+//city UV-Index link to html
+var cityUVIndex = document.querySelector('UV-Index');
